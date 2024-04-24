ed015e13cfc63840e33e95264448f3635340cd97
Fel <felgonzalez951@gmail.com>
2024-04-24 13:01:03 -0600

    added another question to the script prompt

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ inquirer
             name:'name',
             message: "What's your name?",
             type: 'input'
+        },
+        {
+            name: 'hobby',
+            message: "What's your favorite hobby?",
+            type: 'input'
         }
     ])
     .then(function(answer){
